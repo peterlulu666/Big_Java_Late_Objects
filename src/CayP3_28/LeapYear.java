87473bf3c8a2e8da1eb0bc8e82c8bdba388cb871
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-12-09 11:32:16 -0500

    commit

--- src/CayP3_28/LeapYear.java
+++ src/CayP3_28/LeapYear.java
@@ -12,16 +12,18 @@ public class LeapYear {
     }
 
     public void checkLeapYear() {
-        if (this.year % 100 == 0) {
-            if (this.year % 400 == 0) {
-                System.out.println("leap year");
-            } else {
-                System.out.println("It is not a leap year");
-            }
-        } else if (this.year % 4 == 0) {
-            System.out.println("leap year");
+        if (year % 4 != 0) {
+            System.out.println("It is not a leap year. ");
+
+        } else if (year % 100 != 0) {
+            System.out.println("It is a leap year. ");
+
+        } else if (year % 400 != 0) {
+            System.out.println("It is not a leap year.");
+
         } else {
-            System.out.println("It is not a leap year");
+            System.out.println("It is a leap year. ");
+
         }
     }
 }
